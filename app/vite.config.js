c2899ed46d69e958d64fc5ad1bd37ac844685346
anhtuan43 <159206392+anhtuan43@users.noreply.github.com>
2025-06-17 13:47:55 +0700

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': {
+        target: 'http://localhost:9098',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
-};
+});
